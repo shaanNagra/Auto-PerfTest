b3018896a0f78c28a2f2bb72e644119d060a7ea7
shaanNagra <shaan.n@outlook.co.nz>
2021-08-16 17:01:43 +1200

    created base foundation comparison for list of cluster

--- Test-Builder/Analyzer/group/comparison.py
+++ Test-Builder/Analyzer/group/comparison.py
@@ -11,7 +11,6 @@ def clusterDifference(cluster1,cluster2):
                 found.append(j)
                 break
 
-        
     Difference += c1Len-len(found)
     Difference += c2Len-len(found)
     # total = all uniqe items plus items that occured in both list
@@ -31,7 +30,33 @@ def clusterSimilarity(cluster1,cluster2):
 
 
 
+def clusterMulti(clusterList):
+    length = 0
+    counter = {}
+    for cluster in clusterList:
+        length +=1
+        foundInCluster = {}
+        # l = 0
+        for i in range(len(cluster)):
+            # l +=1 
+            # print(l)
+            operation = cluster[i]['operation']
 
+            if operation in foundInCluster:
+                foundInCluster[operation] += 1
+            else:
+                foundInCluster[operation] = 1
+            
+            operation += ' '+str(foundInCluster[operation])
+            # print(operation)
+            if operation in counter:
+                counter[operation] += 1
+            else:
+                counter[operation] = 1
+    
+    print(length)
+    for k,v in counter.items():
+        print(str(k) +'  =>   '+ str(v))
 # l1 = ['m','b','d','i','i','i']
 # l2 = ['c','i','a','d','d','t']
 
