c993790adf1cf19a55bd4aadf41ed4f7c1315475
shaanNagra <shaan.n@outlook.co.nz>
2021-08-23 19:18:04 +1200

    grouping across sessions is complete

--- test_builder/session_grouper/comparison.py
+++ test_builder/session_grouper/comparison.py
@@ -1,44 +1,58 @@
+#!/usr/bin/env python
 
-def clusterDifference(cluster1,cluster2):
-    c1Len = len(cluster1)
-    c2Len = len(cluster2)
+
+# ------------------------------------------
+#
+# ------------------------------------------
+def clusterDifference(clstr1, clstr2):
+    c1Len = len(clstr1)
+    c2Len = len(clstr2)
     total = c1Len + c2Len
     Difference = 0
     found = []
     for i in range(c1Len):
         for j in range(c2Len):
-            if cluster1[i]['operation'] == cluster2[j]['operation'] and j not in found:
+            if clstr1[i]['operation'] == clstr2[j]['operation'] and \
+               j not in found:
+
                 found.append(j)
                 break
 
     Difference += c1Len-len(found)
     Difference += c2Len-len(found)
     # total = all uniqe items plus items that occured in both list
-    # l1 = (a b c d)   l2 = (a d g)    then  total = length of set (a b c d g)  
+    # l1=(a b c d) l2=(a d g) then total=length of set(a b c d g)
     return Difference/(total-len(found))*100.0
 
-def clusterSimilarity(cluster1,cluster2):
+
+# ------------------------------------------
+#
+# ------------------------------------------
+def clusterSimilarity(cluster1, cluster2):
     total = len(cluster1)+len(cluster2)
     # Difference = 0
     found = []
     for i in range(len(cluster1)):
         for j in range(len(cluster2)):
-            if cluster1[i]['operation'] == cluster2[j]['operation'] and j not in found:
+            if cluster1[i]['operation'] == cluster2[j]['operation'] and \
+               j not in found:
                 found.append(j)
                 break
     return (len(found)*2)/total*100.0
 
 
-
+# ------------------------------------------
+# 
+# ------------------------------------------
 def clusterMulti(clusterList):
     length = 0
     counter = {}
     for cluster in clusterList:
-        length +=1
+        length += 1
         foundInCluster = {}
         # l = 0
         for i in range(len(cluster)):
-            # l +=1 
+            # l +=1
             # print(l)
             operation = cluster[i]['operation']
 
@@ -46,20 +60,18 @@ def clusterMulti(clusterList):
                 foundInCluster[operation] += 1
             else:
                 foundInCluster[operation] = 1
-            
+
             operation += ' '+str(foundInCluster[operation])
             # print(operation)
             if operation in counter:
                 counter[operation] += 1
             else:
                 counter[operation] = 1
-    
+
     print(length)
-    for k,v in counter.items():
-        print(str(k) +'  =>   '+ str(v))
+    for k, v in counter.items():
+        print(str(k) + '  =>   ' + str(v))
 
-
-        
 # l1 = ['m','b','d','i','i','i']
 # l2 = ['c','i','a','d','d','t']
 
--- test_builder/session_grouper/grouping.py
+++ test_builder/session_grouper/grouping.py
@@ -10,122 +10,210 @@ from . import comparison
 #
 # ------------------------------------------
 def GroupSessionsViaVersion(webappID, version):
-    sessions = {}
-    innerGroups = {}
-    selectedSessions = DBI.getClusteredSessions(webappID, version)
+    listOfSessions = {}
+    localGroups = {}
 
-    print(selectedSessions)
+    selectedSessions = DBI.getClusteredSessions(webappID, version)
+    # print(selectedSessions)
 
     if len(selectedSessions) >= 1:
         for sID in selectedSessions:
-            sessions[sID] = DBI.getSessionsForGrouping(sID)
-            innerGroups[sID] = similarClustersInSession(sessions[sID])
+            listOfSessions[sID] = DBI.getSessionsForGrouping(sID)
+            localGroups[sID] = findGrpsInSessions(listOfSessions[sID])
     else:
         print("OUT: no results found")
+        return
 
-    for i in innerGroups:
-        print(i)
-        print(innerGroups[i])
+    # for lg in localGroups:
+        # print(lg)
+        # print(localGroups[lg])
 
-    for s1 in sessions:
-        for s2 in sessions:
-            if s1 is not s2:
+    listOfGroups = list()
+    for sessions in listOfSessions:
 
-                pass
-            pass
+        if len(listOfGroups) == 0:
+
+            for groups in localGroups[sessions]:
+                listOfGroups.append({sessions: groups})
+
+        else:
+            for groups in localGroups[sessions]:
+                listOfGroups = findsimilar(
+                    listOfGroups, groups, listOfSessions, sessions
+                    )
+                # print(groups)
+                # print(sessions)
+                # if findsimilar(
+                #      listOfGroups, groups, listOfSessions, sessions
+                #      ) is True:
+                #     listOfGroups[sessions].append({sessions:groups})
+                # else:
+                #     pass
+    print("FINALLRESULTS\n\n\n\n")
+    print(listOfGroups)
 
 
 # ------------------------------------------
 #
 # ------------------------------------------
-def similarClustersInSession(session):
-    e = 1
-    results = set()
-    # print(session)
+def findsimilar(listOfGroups, grpToMatch, loS, g2mSID):
+    print(listOfGroups)
+    for i in range(len(listOfGroups)):
+        if matchedAllInGroup(listOfGroups[i], grpToMatch, loS, g2mSID) is True:
+            print(i)
+            listOfGroups[i][g2mSID] = grpToMatch
+            return listOfGroups
+    listOfGroups.append({g2mSID: grpToMatch})
+    return listOfGroups
+    #    # print('----------------------------------------------')
+    #    # print(grouping)
+    #    # print("----------------------------------------------")
+    #    # for singleGrp in grouping:
+    #        # for grp in grpToMatch:
+    #            # print(grp)
+    #            # print(listOfSessions[grp])
+    #            # return
+    #            # break
+    #            # print(grouping[singleGrp])
+    #    # pass
 
-    listOfKeys = list(session.keys())
 
-    pointerDict = {}
-    for i in session:
-        pointerDict[i] = []
+# {tok1:[2,3],tok2:[1],tok3:[9]}
+def matchedAllInGroup(grouping, grpToMatch, loS,  g2mSID):
+    for singleGrp in grouping:
+        if matchedInLocalGroups(
+             grouping[singleGrp], grpToMatch, loS[singleGrp], loS[g2mSID]
+             ) is False:
+            return False
+    return True
 
-    # print(listOfKeys)
-    for i in session:
-        for j in session:
 
-            # printOpsAcrossTwoSession(session[i],session[j])
+def matchedInLocalGroups(grp, grp2match, ops, ops2):
+    for clstr in grp:
+        for clstr2 in grp2match:
+            # print(str(clstr)+"    "+str(clstr2))
+            # print(ops[clstr])
+            # print(ops2[clstr2])
+            if clstrMatched(ops[clstr], ops2[clstr2]) is False:
+                return False
+    return True
 
-            # NOT the same cluster
-            if j is not i:
 
-                # DIFF IS LESS THAN 40%
-                diff = comparison.clusterDifference(session[i], session[j])
-                if diff <= 40:
-                    # SIM IS MORE THAN 80%
-                    sim = comparison.clusterSimilarity(session[i], session[j])
-                    if sim >= 80:
-                        # ADD COMBO OF CLUSTERS
-                        # print(str(listOfKeys[i])+" & "+str(listOfKeys[j])+": sim = "+str(sim)+" diff = "+str(diff))
-                        # t = tuple(sorted([listOfKeys[i], listOfKeys[j]]))
-                        # results.add(t)
-                        if i not in pointerDict[j]:
-                            pointerDict[j].append(i)
+# ------------------------------------------
+#
+# ------------------------------------------
+def temp2():
+    pass
 
-            # print(str(e)+"   diff = "+str(diff))
-            # print("    sim = "+str(sim))
-            e += 1
-    # print("=====================")
-    # print(pointerDict)
 
-    pointerList = []
+# ------------------------------------------
+#
+# ------------------------------------------
+def clstrMatched(clstr1, clstr2):
+
+    # NOT the same cluster
+    if clstr1 is not clstr2:
+
+        # DIFF IS LESS THAN 40%
+        diff = comparison.clusterDifference(clstr1, clstr2)
+        if diff <= 40 :
+            # SIM IS MORE THAN 80%
+            sim = comparison.clusterSimilarity(clstr1, clstr2)
+            if sim >= 80:
+                return True
+    return False
+
+
+# ------------------------------------------
+#
+# ------------------------------------------
+def convertPointersDictToList(pointerDict):
+    groupsList = []
     for key in pointerDict:
         pointerDict[key].append(key)
-        pointerList += [sorted(pointerDict[key])]
+        groupsList += [sorted(pointerDict[key])]
+    return groupsList
 
-    # print("\n")
-    # print(sorted(pointerList))
-    # print('\n')
+
+# ------------------------------------------
+#
+# ------------------------------------------
+def removeDuplicates(listwithDups):
     res = []
-    for i in pointerList:
+    for i in listwithDups:
         if i not in res:
             res.append(i)
-    # print(res)
+    return res
+
+
+# ------------------------------------------
+#
+# ------------------------------------------
+def removeSetwithSubsets(groupsList):
 
     matches = {}
-    for i in range(len(res)):
-        for j in range(len(res)):
-            if len(res[i]) < len(res[j]):
+
+    for i in range(len(groupsList)):
+        for j in range(len(groupsList)):
+
+            if len(groupsList[i]) < len(groupsList[j]):
                 subset = True
                 # print(str(i) + "   " + str(j))
-                for ii in res[i]:
-                    if ii not in res[j]:
+
+                for ii in groupsList[i]:
+                    if ii not in groupsList[j]:
                         subset = False
 
                 if subset is True:
                     if j not in matches:
                         matches[j] = []
-                    matches[j].extend(res[i])
+                    matches[j].extend(groupsList[i])
                 subset = True
     # print(matches)
-    
+    return dropSetswithSubset(groupsList, matches)
+
+
+# ------------------------------------------
+#
+# ------------------------------------------
+def dropSetswithSubset(groupsList, matches):
     dontdrop = set()
     for i in matches:
         flag = True
-        for j in res[i]:
+        for j in groupsList[i]:
             if j not in matches[i]:
                 dontdrop.add(i)
                 flag = False
         if flag is True:
-            res.pop(i)
+            groupsList.pop(i)
+    return(groupsList)
 
-    # print(dontdrop)
 
-    # for i in matches:
-    #     if i not in dontdrop:
-    #         res.pop(i)
-    print(list(session.keys()))
-    print(res)
-    return results
+# ------------------------------------------
+#
+# ------------------------------------------
+def findGrpsInSessions(sessions):
+
+    # listOfKeys = list(sessions.keys())
+
+    pointerDict = {}
+    for i in sessions:
+        pointerDict[i] = []
+
+    # print(listOfKeys)
+    for i in sessions:
+        for j in sessions:
+            if clstrMatched(sessions[i], sessions[j]) is True:
+                if i not in pointerDict[j]:
+                    pointerDict[j].append(i)
+
+    groupsList = convertPointersDictToList(pointerDict)
+    groupsList = removeDuplicates(groupsList)
+    groupsList = removeSetwithSubsets(groupsList)
+
+    print(list(sessions.keys()))
+    print(groupsList)
+    return groupsList
 
 
 # ------------------------------------------
